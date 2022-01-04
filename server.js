5106cd72a9d77fb998e8dbacfb2fce263716406c
santosh <intelligencechannel86@gmail.com>
2022-01-04 17:43:06 +0545

    starting now backend

--- server.js
+++ server.js
@@ -5,13 +5,24 @@ const expressLayout = require("express-Ejs-layouts");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
 //assets
 app.use(express.static("public"))
 //set Template Engine
 app.use(expressLayout);
 app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
+
+//Routes
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/cart",(req,res)=>{
+  res.render("customer/cart")
+})
+app.get("/register",(req,res)=>{
+  res.render("auth/register")
+})
+app.get("/login",(req,res)=>{
+  res.render("auth/login")
+})
 app.listen(PORT, () => console.log(`server started at ${PORT}`));
